9e0df6b1893eb0e78fdd31d019a5edbaed1f31a9
taizelcg@gmail.com <taizelcg@gmail.com@06f4308e-098f-11de-a1f0-819f45317607>
2009-03-05 14:17:11 +0000

    Gestor Contábil - Primeira submissão.

---
+++ trunk/Gestor Contabil/src/gestorcontabil/Main.java
@@ -0,0 +1,12 @@
+package gestorcontabil;
+
+import janelas.MenuInicial;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MenuInicial m = new MenuInicial();
+        m.setVisible(true);
+    }
+
+}
