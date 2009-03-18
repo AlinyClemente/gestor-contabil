0ecd8900543f7f6f17c76b51d36f09377a832f80
taizelcg@gmail.com <taizelcg@gmail.com@06f4308e-098f-11de-a1f0-819f45317607>
2009-03-18 19:00:23 +0000

    Atualização 18-03

---
+++ trunk/Gestor Contabil/src/cadastros/Cliente.java
@@ -0,0 +1,18 @@
+package cadastros;
+
+import enums.Sexo;
+import java.util.Date;
+
+/**
+ *
+ * @author Táizel Girão
+ */
+public class Cliente {
+
+    private int codigo;
+    private String nome;
+    private Sexo sexo;
+    private Date dataNascimento;
+    
+
+}
